71a0b2909ac9239fdddbdc879c57f076de7f3846
abdiasmajbellorin-lab <abdias.majbellorin@escola.pr.gov.br>
2026-06-18 08:31:09 -0300

    Add files via upload
    }
    
    
    hbhhijhjijiojjoiohugfyygtuy76dfyuutrstesdrftghjklkjhgfdsdfghjkl=-098890-/*

---
+++ java2.js
@@ -0,0 +1,16 @@
+//2
+let a = 10;
+let b = 5;
+let c;
+
+c = a + b;
+console.log("soma:",c);
+
+c = a - b;
+console.log("subtracao:",c);
+
+c = a * b;
+console.log("multiplicacao",c);
+
+c = a / b;
+console.log("divisao:",c);
